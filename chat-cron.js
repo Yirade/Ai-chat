386583eb57759e0463dbcec3ef69ba6ea60d9f1f
Yirade <gennyeco2002wiko@gmail.com>
2023-01-03 23:26:16 +0200

    Update

--- chat-cron.js
+++ chat-cron.js
@@ -15,12 +15,10 @@ function encodeHTML(s) {
 }
 
 function sendMessage(event) {
-  // Aggiungi questa riga per impedire l'invio del modulo
   event.preventDefault();
   if (input.value.trim()) {
     const message = input.value;
     chat.innerHTML += `<div class="chat-bubble user-bubble"><small class="text-muted" id="usersm">You:</small><span class="mx-2">${message}</span></div>`;
-    // Show the loading spinner
     document.querySelector('.loading').style.display = 'block';
     input.value = '';
     chat.scrollTop = chat.scrollHeight;
@@ -44,31 +42,22 @@ async function ai_bot(prompt_text) {
       console.log(xhr.status);
       console.log(xhr.responseText);
 
-      // Parse the response string and get the text field
       const responseObject = JSON.parse(xhr.responseText);
       const text = responseObject.choices[0].text;
 
-      // Trim the leading \n characters
       trimmed = text.trimStart();
 
-      // Replace all occurrences of \n with <br>
-      // Use a <pre> element to preserve the text formatting
       const replaced = `<pre>${encodeHTML(trimmed)}</pre>`;
 
-
-      // Hide the loading spinner
       document.querySelector('.loading').style.display = 'none';
 
-      // Append the chat bubbles to the UI
       chat.innerHTML += `<div class="chat-bubble bot-bubble"><span>${replaced}</span></div>`;
       chat.scrollTop = chat.scrollHeight;
 
-      // Update the previous responses
       previousResponses += `${message}\\n\\n`;
     }
   };
 
-  // Include previous responses in the prompt
   var data = `{
     "prompt": "${"previous questions: " + previousResponses + "\\n\\nactual question: " + prompt_text + "\\n\\nanswer: "}",
     "temperature": 0.7,
